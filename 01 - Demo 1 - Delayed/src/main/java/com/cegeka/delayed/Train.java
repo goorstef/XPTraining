a7661512942baa53e21f0223aa8a3e671098a60d
Vincent Verhoeven <verhoevenv@gmail.com>
2015-10-30 16:50:01 +0100

    Initial for Delayed

--- 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
+++ 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
@@ -1,17 +1,16 @@
 package com.cegeka.delayed;
 
 public class Train {
-    private int hoursDelayed;
 
     public Train(int hoursDelayed) {
-        this.hoursDelayed = hoursDelayed;
+
     }
 
     public int getHoursDelayed() {
-        return hoursDelayed;
+
     }
 
     public void setHoursDelayed(int hoursDelayed) {
-        this.hoursDelayed = hoursDelayed;
+
     }
 }
