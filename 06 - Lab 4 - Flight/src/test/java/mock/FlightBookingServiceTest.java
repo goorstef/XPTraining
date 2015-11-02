afcaeb1addd6cfc40af0eb9c40569183a4d5c2a6
Vincent Verhoeven <verhoevenv@gmail.com>
2015-11-02 21:29:29 +0100

    Upgrade Flight to assertj

--- 06 - Lab 4 - Flight/src/test/java/mock/FlightBookingServiceTest.java
+++ 06 - Lab 4 - Flight/src/test/java/mock/FlightBookingServiceTest.java
@@ -1,16 +1,14 @@
 package mock;
 
-import static junit.framework.Assert.assertTrue;
-
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class FlightBookingServiceTest {
-	
+
 	private FlightBookingServiceImpl flightBookingServiceImpl;
 	private PersistenceManagerMock<Flight> persistenceManagerMock;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		persistenceManagerMock = new PersistenceManagerMock<Flight>();
@@ -21,10 +19,10 @@ public class FlightBookingServiceTest {
 	public void bookSeatOnFlight() throws FlightFullyBookedException {
 		Flight flight = new FlightImpl(12);
 		persistenceManagerMock.expectLookup(Flight.class, 12, flight);
-		
+
 		flightBookingServiceImpl.bookSeatOnFlight(12, "persoon");
-		
-		assertTrue(flight.isPassengerOnFlight("persoon"));
+
+		Assertions.assertThat(flight.isPassengerOnFlight("persoon")).isTrue();
 		persistenceManagerMock.verify();
 	}
 
