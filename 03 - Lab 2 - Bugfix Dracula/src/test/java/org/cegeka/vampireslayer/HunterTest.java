3bd7039f050c7f59959c31186a516994548f1436
Vincent Verhoeven <verhoevenv@gmail.com>
2015-11-02 16:30:51 +0100

    Updated vampireslayer to latest AssertJ

--- 03 - Lab 2 - Bugfix Dracula/src/test/java/org/cegeka/vampireslayer/HunterTest.java
+++ 03 - Lab 2 - Bugfix Dracula/src/test/java/org/cegeka/vampireslayer/HunterTest.java
@@ -1,40 +1,37 @@
 package org.cegeka.vampireslayer;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HunterTest {
-    private Hunter hunter;
+	private Hunter hunter;
 
-    @Before
-    public void setup()
-    {
-        hunter = new Hunter();
-    }
+	@Before
+	public void setup() {
+		hunter = new Hunter();
+	}
 
-    @Test
-    public void canHunt_givenDayTime_thenCannotHunt()
-    {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2014, Calendar.APRIL, 12, 16, 1);
-        Date huntingTime = calendar.getTime();
+	@Test
+	public void canHunt_givenDayTime_thenCannotHunt() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2014, Calendar.APRIL, 12, 16, 1);
+		Date huntingTime = calendar.getTime();
 
-        assertThat(hunter.canHunt(huntingTime)).isFalse();
-    }
+		assertThat(hunter.canHunt(huntingTime)).isFalse();
+	}
 
-    @Test
-    public void canHunt_givenNightTime_thenCanHunt()
-    {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2014, Calendar.APRIL, 12, 1, 1);
-        Date huntingTime = calendar.getTime();
+	@Test
+	public void canHunt_givenNightTime_thenCanHunt() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2014, Calendar.APRIL, 12, 1, 1);
+		Date huntingTime = calendar.getTime();
 
-        assertThat(hunter.canHunt(huntingTime)).isTrue();
-    }
+		assertThat(hunter.canHunt(huntingTime)).isTrue();
+	}
 
 }
